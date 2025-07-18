baee4d23eb9e825528b4acfb3403777af45f89c7
theimranshaik <shaik2022imran.aim@gmail.com>
2025-07-18 12:34:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.5/+esm';
+
+const supabaseUrl = 'https://wectlainkathzkumjfmo.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndlY3RsYWlua2F0aHprdW1qZm1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4MjExNzQsImV4cCI6MjA2ODM5NzE3NH0.jhs8bFxDu8xS2uHw_ZzE6z47RZ717UIp2sDXVx98CM8';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Minimal fetch logic
+async function fetchLeads() {
+  const { data } = await supabase.from("leads").select("*").order("created_at", { ascending: false });
+  console.log(data);
+}
+
+fetchLeads();
